9f93d3659bd394df5bf25db5944235211501f287
Mark A. <mark001616@gmail.com>
2024-12-29 15:03:26 -0500

    Add route to automatically gen 3 pokemons

---
+++ controllers/pokemon.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const getPokemons = async (req, res) => {
+  let pokemons = [];
+  const max = await prisma.pokemon.count();
+
+  for (let x = 0; x < 3; x++) {
+    let randomPkId = getRandomInt(1, max);
+
+    let isPkInArr = pokemons.some((pokemon) => pokemon.id === randomPkId);
+    if (isPkInArr) {
+      console.log("repeating...");
+      x--;
+    } else {
+      const pkToAdd = await prisma.pokemon.findUnique({
+        where: {
+          id: randomPkId,
+        },
+      });
+      pokemons.push(pkToAdd);
+    }
+  }
+
+  res.send(pokemons);
+};
+
+// Helper functions
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+module.exports = { getPokemons };
